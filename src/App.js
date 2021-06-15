b6e819eafe4dd0e77531eb75eb11575c54948246
James McCarron <mccarron.j@gmail.com>
2021-06-15 13:51:38 +1000

    Portals added to ErrorModal.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
+
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -15,10 +16,10 @@ const App = () => {
   };
 
   return (
-    <>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </>
+    </Fragment>
   );
 };
 
